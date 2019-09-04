dcc6731d99745ac20f999056b2fa46983556cc94
dung nguyen <dungnguyen@dungs-MacBook-Pro-2.local>
2019-09-04 12:41:43 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Editor HTML5
+
+1. npm i
+2. npm run dev
+
+open index.html
--- modules/image.js
+++ modules/image.js
@@ -1,14 +1,33 @@
-import { exec } from './../utils/core';
 import { insertImage } from './../enum/element';
-
+import { createElement } from './../utils/core';
+let selectCurrent = null;
+function dd() {
+  let range;
+  if (window.getSelection && window.getSelection().getRangeAt) {
+    range = window.getSelection().getRangeAt(0);
+    selectCurrent = range;
+  }
+}
 function handler(e) {
+  console.log(selectCurrent);
   const image = prompt('image');
-  exec('insertimage', image);
+  return document.execCommand(
+    'insertImage',
+    false,
+    'https://kenh14cdn.com/thumb_w/620/2019/9/4/goo-hye-sun-ahn-jae-hyun-1567411597981712284395-1567567724940533410690.jpg'
+  );
+  const images = {
+    name: 'img',
+    prop: {
+      attribute: [['src', image]]
+    }
+  };
+  const ig = createElement(document.body, images);
+  selectCurrent.insertNode(ig);
 }
 
 function imageSelectHandler(e) {
   const image = e.path[0];
-  const context = document.querySelectorAll('.fwt-editor')[0];
   if (e.target.nodeName === 'IMG') {
     const selection = window.getSelection();
     const range = document.createRange();
@@ -20,7 +39,8 @@ function imageSelectHandler(e) {
 
 insertImage.prop.event = [
   ['click', 1, handler],
-  ['click', 0, imageSelectHandler]
+  ['click', 0, imageSelectHandler],
+  ['mouseup', 0, dd]
 ];
 
 export { insertImage };
